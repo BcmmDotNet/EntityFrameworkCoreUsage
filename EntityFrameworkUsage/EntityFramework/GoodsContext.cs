69abb9bed36f955d53a15c855a01bdc8b6ab7cd7
兰小方 <13604461032@163.com>
2022-10-23 23:34:06 +0800

    first commit

---
+++ EntityFrameworkUsage/EntityFramework/GoodsContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace EntityFrameworkUsage.EntityFramework
+{
+    public class GoodsContext : DbContext
+    {
+        public DbSet<Goods>? Goodses { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Server=(localdb)\mssqllocaldb;Database=EntityFrameworkUsage");
+        }
+    }
+}
---
+++ EntityFrameworkUsage/Program.cs
@@ -0,0 +1,18 @@
+﻿using EntityFrameworkUsage.EntityFramework;
+using Microsoft.EntityFrameworkCore.ChangeTracking;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        using GoodsContext context = new GoodsContext();
+
+        EntityEntry<Goods> addResult = context.Add(new Goods()
+        {
+            Id = new Guid(),
+            Name = "firstName"
+        });
+
+        var result = context.SaveChanges();
+    }
+}
